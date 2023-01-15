8e5a7ecdfe69d3d8e19f0999ae326e97d12c86eb
Fernando Maia <fermaiasoares@aol.com>
2023-01-15 20:17:30 -0300

    Firebase Dependencias

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'com.versatec.acs',
+  id: 'com.versatec.versasus',
   appPath: 'src',
   appResourcesPath: 'App_Resources',
   android: {
